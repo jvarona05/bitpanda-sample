ba6954081836c3f14e8095f9b03961da24969d7c
José <jose.varona05@gmail.com>
2020-04-03 16:10:52 +0200

    add more columns to users migrations

---
+++ database/migrations/2020_04_03_134145_add_more_columns_to_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMoreColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('verified');
+            $table->string('mobile');
+            $table->boolean('mobile_verified');
+            $table->date('birthday');
+            $table->string('street');
+            $table->string('zipcode');
+            $table->string('iso');
+            $table->foreignId('country_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'verified', 
+                'mobile', 
+                'mobile_verified',
+                'birthday',
+                'street',
+                'zipcode',
+                'iso',
+                'country_id'
+            ]);
+        });
+    }
+}
