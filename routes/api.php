4f3205ce412ef4e0b4d426dec6b65dec18672735
José <jose.varona05@gmail.com>
2020-04-04 16:16:35 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::prefix('wallets')->group(function () {
+        Route::get('/', 'WalletController@index');
+    });
 });
-
-Route::get('/test', 'TestController@test');
